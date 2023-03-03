e581b92c9682dc025960542982646bb77595fd12
adityaekafernanda2109 <ekafernandaaaaa@gmail.com>
2023-03-03 16:44:24 +0800

    update and final model

--- app/Models/ManagementAccess/Role.php
+++ app/Models/ManagementAccess/Role.php
@@ -26,4 +26,14 @@ class Role extends Model
          'updated_at',
          'deleted_at',
      ];
+
+     public function role_user()
+     {
+        return $this->hasMany('App\Models\ManagementAccess\RoleUser', 'role_id');
+     }
+
+     public function permission_role()
+     {
+        return $this->hasMany('App\Models\ManagementAcces\PermissionRole', 'role_id');
+     }
 }
